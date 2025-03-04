07fb0bec5653f399b69705cec0456f694c28ce47
HopeJBG <sadejnbaptiste@hotmail.com>
2025-03-04 18:41:17 -0500

    Added testIndices, TDD

--- src/main/java/travis_ci_tutorial_java/SimpleCalculator.java
+++ src/main/java/travis_ci_tutorial_java/SimpleCalculator.java
@@ -1,3 +1,13 @@
+/**
+ * --------------------------------------------------------------
+ * SimpleCalculatorTest.java
+ * Edited By: Sadé Jn Baptiste
+ * Course: COSC 603
+ * Date: March 4, 2025
+ * Description: This file implements a simple calculator with add, subtract,
+ *  multiply, divide, and error handling for division by zero.
+ * --------------------------------------------------------------
+ */
 package travis_ci_tutorial_java;
 
 public class SimpleCalculator {
@@ -22,5 +32,9 @@ public class SimpleCalculator {
 			throw new ArithmeticException("Cannot divide by zero");
 		}
 	}
+
+	//public int indices( int a, int b){
+	//return a^b;
+	//}
 
 }
--- src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
+++ src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
@@ -1,3 +1,13 @@
+/**
+ * --------------------------------------------------------------
+ * SimpleCalculatorTest.java
+ * Edited By: Sadé Jn Baptiste
+ * Course: COSC 603
+ * Date: March 4, 2025
+ * Description: This file implements tests for SimpleCalculator.java
+ * --------------------------------------------------------------
+ */
+
 package travis_ci_tutorial_java;
 
 import java.util.*;
@@ -34,5 +44,20 @@ public class SimpleCalculatorTest {
 	public void testDivideByZero() {
 		SimpleCalculator calc = new SimpleCalculator();
 		assertThrows(ArithmeticException.class, () -> calc.divideZero(10, 0));
+	}
+
+	@Test
+	public void testNegativeValues(){
+		SimpleCalculator calc= new SimpleCalculator();
+		assertEquals(calc.add(-8,-6),-14);
+		assertEquals(calc.subtract(-10, -7),-3);
+		assertEquals (calc.multiply( -7 , -4),28);
+		assertEquals (calc.divide(-10, -5),2);
+	}
+
+	@Test
+	public void testIndicies(){
+		SimpleCalculator calc = new SimpleCalculator();
+		assertEquals(calc.indices(5,3),125);
 	}
 }
